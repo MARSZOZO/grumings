cc30cf0a245af91955f24900391a6bbc5cbe16b8
Marsel Sultanov <marsel.sultanov.zozo@gmail.com>
2021-08-16 02:08:32 +0300

    ufff new navbar

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,4 @@
+
 function changeHeight() {
     let filterBox = document.querySelector('.filter-box'),
         filterBoxButtonShowAllIcon = document.querySelector('.filter-arrow'),
@@ -7,46 +8,21 @@ function changeHeight() {
     filterBox.classList.toggle('show-all')
     filterBoxButtonClear.classList.toggle('d-none')
 
-    if(filterBox.classList.contains('show-all')) {
+    if (filterBox.classList.contains('show-all')) {
         filterBoxButtonShowAll.innerHTML = 'Свернуть'
         filterBoxButtonShowAllIcon.style
-    }else{
+    } else {
         filterBoxButtonShowAll.innerHTML = 'Показать больше фильтров <i class="bx bx-chevron-down filter-arrow"></i>'
     }
 
     filterBoxButtonShowAllIcon.classList.toggle('bx-rotate-180')
 }
 
-
-
-// search-box open close js code
-let navbar = document.querySelector(".navigation-bar");
-
-// sidebar open close js code
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navigation-bar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function () {
-    navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function () {
-    navLinks.style.left = "-100%";
-}
-
-
-// sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function () {
-    navLinks.classList.toggle("show1");
-}
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function () {
-    navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function () {
-    navLinks.classList.toggle("show3");
-}
-
-
-
+window.addEventListener('resize', function(event){
+    let bigText = document.querySelector('.big-text')
+    if(event.target.innerWidth < 1200) {
+        bigText.innerHTML = 'Вет. услуги'
+    }else{
+        bigText.innerHTML = 'Ветеринарные услуги'
+    }
+});
